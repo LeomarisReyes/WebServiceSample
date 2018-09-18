30f7f6a6629012cfb02cdcb8e14434c9079021da
Leomaris Reyes <reyes.leomaris@gmail.com>
2018-09-18 19:35:52 -0400

    Web Service Sample

---
+++ WebServiceSample/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WebServiceSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+             
+        }
+
+		private async void BtnCallWS_Click (object sender, EventArgs e)    
+        {    
+                WSClient client = new WSClient();
+                var result = await client.Get<WSResult>("https://jsonplaceholder.typicode.com/posts/1");
+                string h = string.Empty;
+
+                if (result != null)
+                {
+                    lblId.Text    = $"{result.id}";
+                    lblTitle.Text = result.title;
+                    lblBody.Text  = result.body;
+                }
+ 
+        }  
+
+    }
+}
---
+++ WebServiceSample/WSClient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace WebServiceSample
+{
+    public class WSClient
+    {
+        public async Task<T> Get<T>(string url)
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync(url);
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+    }
+}
